f82f53fb56ee427ca291918bac717be802665555
raajnadar <raajnadar@gmail.com>
2018-09-02 16:10:43 +0530

    Add react-native-paper

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+
+import { DefaultTheme, Provider as PaperProvider, Appbar, Button, Text } from 'react-native-paper'
 
 export default class App extends Component {
+	constructor(props) {
+		super(props)
+
+		this.buttonClick = this.buttonClick.bind(this)
+	}
+
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text>Guess the number</Text>
-			</View>
+			<PaperProvider theme={DefaultTheme}>
+				<Appbar.Header>
+				<Appbar.Content
+					title="Guess It"
+				/>
+				</Appbar.Header>
+				<Text style={styles.title}>Let's assume the random number is X</Text>
+				<View style={styles.container}>
+					<Text style={{ fontSize: 22, marginHorizontal: 10, marginBottom: 20 }}>X is greater than 0</Text>
+					<View style={styles.padder}>
+						<Button mode="contained" onPress={this.buttonClick}>+</Button>
+					</View>
+					<View style={styles.padder}>
+						<Text style={{ fontSize: 22 }}>0</Text>
+					</View>
+					<View style={styles.padder}>
+						<Button mode="contained" onPress={this.buttonClick}>-</Button>
+					</View>
+					<View style={styles.padder}>
+						<Button mode="contained" onPress={this.buttonClick}>Guess</Button>
+					</View>
+				</View>
+			</PaperProvider>
 		)
+	}
+
+	buttonClick() {
+		console.log('Hello')
 	}
 }
 
@@ -17,5 +49,14 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	title: {
+		fontSize: 22,
+		textAlign: 'center',
+		paddingHorizontal: 20,
+		marginTop: 10
+	},
+	padder: {
+		paddingBottom: 16
 	}
 })
