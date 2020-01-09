7fac7150ee02f153840b581ae61cddce15c8ce4f
raajnadar <raajnadar@gmail.com>
2020-01-09 15:03:06 +0530

    fix: statusbar color

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper'
+import {
+	DefaultTheme,
+	Provider as PaperProvider,
+	useTheme
+} from 'react-native-paper'
+
+import { StatusBar } from 'react-native'
 
 import { NavigationContainer } from '@react-navigation/native'
 
 import Router from './router/Router'
 
 export default function App(): JSX.Element {
+	const theme = useTheme()
+
+	useEffect(() => {
+		StatusBar.setBackgroundColor(theme.colors.primary)
+	})
+
 	return (
 		<PaperProvider theme={DefaultTheme}>
 			<NavigationContainer>
