ff042c312935feaf913fb2b0c91cefa3575e38e8
raajnadar <raajnadar@gmail.com>
2020-01-13 15:31:21 +0530

    fix: rename bundle identifier

--- android/app/src/main/java/in/raajnadar/guessit/MainActivity.java
+++ android/app/src/main/java/in/raajnadar/guessit/MainActivity.java
@@ -1,4 +1,4 @@
-package com.guessit;
+package in.raajnadar.guessit;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/in/raajnadar/guessit/MainApplication.java
+++ android/app/src/main/java/in/raajnadar/guessit/MainApplication.java
@@ -1,4 +1,4 @@
-package com.guessit;
+package in.raajnadar.guessit;
 
 import android.app.Application;
 import android.content.Context;
